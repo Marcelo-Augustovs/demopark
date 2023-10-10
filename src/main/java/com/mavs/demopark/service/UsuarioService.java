ac7e2143051e19251f780557540e16d0d683bad9
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2023-10-10 14:26:28 -0300

    Adicionada Incrementacao para requisicoes em usuarios

--- src/main/java/com/mavs/demopark/service/UsuarioService.java
+++ src/main/java/com/mavs/demopark/service/UsuarioService.java
@@ -1,12 +1,33 @@
 package com.mavs.demopark.service;
 
+import com.mavs.demopark.entity.Usuario;
 import com.mavs.demopark.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository repository;
+
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return repository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id) {
+        return repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuario não encontrado"));
+    }
+
+    @Transactional
+    public Usuario editarSenha(Long id, String password) {
+        Usuario user = buscarPorId(id);
+        user.setPassword(password);
+        return user;
+    }
 }
