d908d73b21fc7a5b2be6fd7dda9a6b3660f24a70
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2023-10-10 14:21:48 -0300

    Adicionados novos EndPoints para Usuarios

---
+++ src/main/java/com/mavs/demopark/web/controller/UsarioController.java
@@ -0,0 +1,35 @@
+package com.mavs.demopark.web.controller;
+
+import com.mavs.demopark.entity.Usuario;
+import com.mavs.demopark.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/usuarios")
+public class UsarioController {
+
+    private final UsuarioService usuarioService;
+
+
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
+        Usuario user = usuarioService.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id){
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Usuario> updatePassword(@PathVariable Long id , @RequestBody Usuario usuario){
+        Usuario user = usuarioService.editarSenha(id, usuario.getPassword());
+        return ResponseEntity.ok(user);
+    }
+}
