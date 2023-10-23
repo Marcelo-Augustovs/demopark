f2cd945a90c10b8ab73540e0e8064f115e1aea0f
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2023-10-23 14:11:56 -0300

    Adicionado novo dto

---
+++ src/main/java/com/mavs/demopark/web/controller/dto/UsuarioSenhaDto.java
@@ -0,0 +1,15 @@
+package com.mavs.demopark.web.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class UsuarioSenhaDto {
+
+    private String senhaAtual;
+    private String novaSenha;
+    private String confirmarSenha;
+
+}
