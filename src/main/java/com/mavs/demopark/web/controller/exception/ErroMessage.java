10ea8fd7c264d4c408763ad1770826c2966286c0
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2023-11-04 12:01:46 -0300

    Adicionado ExceptionHandler para retornos(exception) personalizados

---
+++ src/main/java/com/mavs/demopark/web/controller/exception/ErroMessage.java
@@ -0,0 +1,53 @@
+package com.mavs.demopark.web.controller.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@ToString
+public class ErroMessage {
+
+    private String path;
+    private String method;
+    private int status;
+    private String statusText;
+    private String message;
+
+    private Map<String,String> erros;
+
+
+    public ErroMessage(){
+
+    }
+
+    public ErroMessage(HttpServletRequest request, HttpStatus status, String message){
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
+        this.message = message;
+    }
+
+    public ErroMessage(HttpServletRequest request, HttpStatus status, String message, BindingResult result){
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
+        this.message = message;
+        addErros(result);
+    }
+
+    public void addErros(BindingResult result){
+        this.erros = new HashMap<>();
+        for (FieldError fieldError : result.getFieldErrors() ){
+            this.erros.put(fieldError.getField(), fieldError.getDefaultMessage() );
+        }
+    }
+}
