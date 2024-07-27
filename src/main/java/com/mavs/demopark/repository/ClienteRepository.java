46771a55be42ed1f177bf94da899dfbbc0c8f123
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2024-07-27 13:18:21 -0300

    adicionado a interface Find(byUsuario e AllPageable)

--- src/main/java/com/mavs/demopark/repository/ClienteRepository.java
+++ src/main/java/com/mavs/demopark/repository/ClienteRepository.java
@@ -1,7 +1,16 @@
 package com.mavs.demopark.repository;
 
 import com.mavs.demopark.entity.Cliente;
+import com.mavs.demopark.repository.projection.ClienteProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ClienteRepository extends JpaRepository<Cliente,Long> {
+
+    @Query("select c from Cliente c")
+    Page<ClienteProjection> findAllPageable(Pageable pageable);
+
+    Cliente findByUsuarioId(long id);
 }
---
+++ src/main/java/com/mavs/demopark/repository/projection/ClienteProjection.java
@@ -0,0 +1,8 @@
+package com.mavs.demopark.repository.projection;
+
+public interface ClienteProjection {
+
+    Long getId();
+    String getNome();
+    String getCpf();
+}
